da9c7933d92e42597b709d0d519da51548e08907
Magnus Dalen Kvalevåg <magnus.kvalevag@gmail.com>
2023-03-17 16:20:29 +0100

    Put simulationParams in state.js

--- js/state.js
+++ js/state.js
@@ -13,3 +13,22 @@ export const uiState = {
     sampleImpulseResponseLineStart: [0.55, 0.2],
     sampleImpulseResponseLineEnd: [0.65, 0.2],
 }
+
+
+export const simulationParams = new Map(
+    [
+        ["time", 0.81],
+        ["arrayCenterX", 0.5],
+        ["arrayCenterY", 1.0],
+        ["focusPointX", 0.6],
+        ["focusPointY", 0.5],
+        ["selectedPointX", 0.6],
+        ["selectedPointY", 0.2],
+        ["waveType", 1],
+        ["centerFrequency", 100],
+        ["pulseLength", 0.0001],
+        ["valueScale", 1],
+        ["valueScaleFactor", 60],
+        ["apertureWindow", 1], // 0 = no weighting, 1 = Tukey weighting
+    ]
+);
